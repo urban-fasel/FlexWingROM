d2a715c5fffa08d68471f5511c3eaa40baa9c8d2
urban-fasel <60729896+urban-fasel@users.noreply.github.com>
2022-01-25 19:54:17 -0800

    Update set_input_unsteadyFSI.m

--- code/FSI/set_input_unsteadyFSI.m
+++ code/FSI/set_input_unsteadyFSI.m
@@ -4,7 +4,6 @@ function simInput = set_input_unsteadyFSI(paramFSI,simUnsteadyParam)
 
 paramFSI.inputCreate.alphaWing = simUnsteadyParam.alpha;
 paramFSI.firstIt = 0;
-paramFSI.cutWakeAt = findCutPositionWake(paramFSI.wingParams,paramFSI.viscPre,simUnsteadyParam.dT);
 
 simInput.paramFSI = paramFSI;
 
@@ -14,11 +13,11 @@ alphaIN0 = 0;                                       % zero aircraft angle of att
 simInput.meanRotationRates = [0,0,0];
 simInput.inputSnapshot0 = [force_input, inputMorphingSymmetric, simInput.meanRotationRates, alphaIN0]; % zero input aDMDc
 
-simInput.inputMorphingSymmetric = 0;                % Morphing actuation input (symmetric -> load alleviation)
-simInput.alphaIN = alphaIN0;                        % Angle of attack at the beginning of the simulation (initialisation phase)
-simInput.force_input = 0;                           % Morphing actuation input (unsymmetric -> roll)
-simInput.Omega = [0,0,0];                           % Rotation rates input
-simInput.deltaAlpha = simUnsteadyParam.deltaAlpha;  % Amplitude angle of attack
+simInput.inputMorphingSymmetric = 0;                    % Morphing actuation input (symmetric -> load alleviation)
+simInput.alphaIN = alphaIN0;                            % Angle of attack at the beginning of the simulation (initialisation phase)
+simInput.force_input = simUnsteadyParam.morphingRoll;   % Morphing actuation input (unsymmetric -> roll)
+simInput.Omega = [0,0,0];                               % Rotation rates input
+simInput.deltaAlpha = simUnsteadyParam.deltaAlpha;      % Amplitude angle of attack
 
 
 k = simUnsteadyParam.k; % reduced frequency
@@ -26,7 +25,12 @@ k = simUnsteadyParam.k; % reduced frequency
 % Alpha signal: Reduced frequency k
 simInput.AlphaShape = sin(2*pi*(k*simUnsteadyParam.V/paramFSI.wingParams.chord*2/2/pi)*simUnsteadyParam.dT*(0:simUnsteadyParam.iTest-1));       
 
-simInput.rotOmega = zeros(1,simUnsteadyParam.iTest);    % Rotation rate signal
-simInput.forceShape1 = zeros(1,simUnsteadyParam.iTest); % Morphing actuation (unsymmetric -> roll)
-simInput.forceShape2 = zeros(1,simUnsteadyParam.iTest); % Morphing actuation (symmetric -> load alleviation)
+% Rotation rate signal
+simInput.rotOmega = zeros(1,simUnsteadyParam.iTest);    
+
+% Morphing actuation (unsymmetric -> roll)
+simInput.forceShape1 = sin(2*pi*(k*simUnsteadyParam.V/paramFSI.wingParams.chord*2/2/pi)*simUnsteadyParam.dT*(0:simUnsteadyParam.iTest-1)); 
+
+% Morphing actuation (symmetric -> load alleviation)
+simInput.forceShape2 = zeros(1,simUnsteadyParam.iTest); 
 
