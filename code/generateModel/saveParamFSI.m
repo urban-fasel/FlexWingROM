9e41f5982de26e53c46a0e0914b0cb8fb3a09ddf
urban-fasel <60729896+urban-fasel@users.noreply.github.com>
2022-01-25 19:53:57 -0800

    Update saveParamFSI.m

--- code/generateModel/saveParamFSI.m
+++ code/generateModel/saveParamFSI.m
@@ -10,33 +10,34 @@ paramFSI.nModes                                 = simParam.nmodes + 2;
 paramFSI.analysisParams.num_airfoil_nodes_panel = simParam.num_airfoil_nodes_panel;
 paramFSI.analysisParams.n_seg_PM                = simParam.n_seg_PM;
 paramFSI.analysisParams.rho                     = simParam.rho;
-paramFSI.analysisParams.simtol                 = simParam.simtol;
+paramFSI.analysisParams.simtol                  = simParam.simtol;
+paramFSI.cutWakeAt                              = simParam.cutWakeAt;
 
 paramFSI.K_MODES    = wingModelStructure.K_MODES;
 paramFSI.M_MODES    = wingModelStructure.M_MODES;
 paramFSI.C_MODES    = wingModelStructure.C_MODES;
 
-paramFSI.y_segLimit       = wingModelAero.y_segLimit      ;
-paramFSI.y_segMidpoint    = wingModelAero.y_segMidpoint   ;
-paramFSI.xMesh_loop       = wingModelAero.xMesh_loop      ;
-paramFSI.yMesh_loop       = wingModelAero.yMesh_loop      ;
-paramFSI.zMesh_loop       = wingModelAero.zMesh_loop      ;
-paramFSI.cLdistr_yPos     = wingModelAero.cLdistr_yPos    ;
+paramFSI.y_segLimit       = wingModelAero.y_segLimit;
+paramFSI.y_segMidpoint    = wingModelAero.y_segMidpoint;
+paramFSI.xMesh_loop       = wingModelAero.xMesh_loop;
+paramFSI.yMesh_loop       = wingModelAero.yMesh_loop;
+paramFSI.zMesh_loop       = wingModelAero.zMesh_loop;
+paramFSI.cLdistr_yPos     = wingModelAero.cLdistr_yPos;
 
-paramFSI.M1               = wingModelAero.M1              ;
-paramFSI.N1               = wingModelAero.N1              ;
-paramFSI.TPSU_X           = wingModelAero.TPSU_X          ;
-paramFSI.TPSU_Y           = wingModelAero.TPSU_Y          ;
-paramFSI.TPSU_Z           = wingModelAero.TPSU_Z          ;
-paramFSI.TPSD_X           = wingModelAero.TPSD_X          ;
-paramFSI.TPSD_Y           = wingModelAero.TPSD_Y          ;
-paramFSI.TPSD_Z           = wingModelAero.TPSD_Z          ;
-paramFSI.IDWuX            = wingModelAero.IDWuX           ;
-paramFSI.IDWuY            = wingModelAero.IDWuY           ;
-paramFSI.IDWuZ            = wingModelAero.IDWuZ           ;
-paramFSI.IDWdX            = wingModelAero.IDWdX           ;
-paramFSI.IDWdY            = wingModelAero.IDWdY           ;
-paramFSI.IDWdZ            = wingModelAero.IDWdZ           ;
+paramFSI.M1               = wingModelAero.M1;
+paramFSI.N1               = wingModelAero.N1;
+paramFSI.TPSU_X           = wingModelAero.TPSU_X;
+paramFSI.TPSU_Y           = wingModelAero.TPSU_Y;
+paramFSI.TPSU_Z           = wingModelAero.TPSU_Z;
+paramFSI.TPSD_X           = wingModelAero.TPSD_X;
+paramFSI.TPSD_Y           = wingModelAero.TPSD_Y;
+paramFSI.TPSD_Z           = wingModelAero.TPSD_Z;
+paramFSI.IDWuX            = wingModelAero.IDWuX;
+paramFSI.IDWuY            = wingModelAero.IDWuY;
+paramFSI.IDWuZ            = wingModelAero.IDWuZ;
+paramFSI.IDWdX            = wingModelAero.IDWdX;
+paramFSI.IDWdY            = wingModelAero.IDWdY;
+paramFSI.IDWdZ            = wingModelAero.IDWdZ;
 paramFSI.viscPre          = wingModelAero.viscPre;
 paramFSI.K_alpha_ind_ELLT = wingModelAero.K_alpha_ind_ELLT;
 
@@ -44,6 +45,7 @@ paramFSI.fiACTMax         = wingModelAero.fiACTMax;
 paramFSI.fiACTMin         = wingModelAero.fiACTMin;
 paramFSI.fiACT_LAMax      = wingModelAero.fiACT_LAMax;
 paramFSI.fiACT_LAMin      = wingModelAero.fiACT_LAMin;
+
 
 %% save parameters for FSI and ROM
 save(['data/parsim_FSI_', wingDesign.airfoil, '.mat'],'paramFSI');
